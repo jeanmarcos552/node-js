e45e37261170e35688afee0f469dc6290c04475c
Jean Marcos <jean.silva552@gmail.com>
2023-05-28 12:06:40 -0300

    test: feat add files testes transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,25 +1,47 @@
-import { test, beforeAll, afterAll } from 'vitest';
+import { beforeAll, afterAll, describe, it, expect } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
 
-beforeAll(async () => {
-  await app.ready();
-});
+describe('Teste Routes transactions', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
 
-afterAll(async () => {
-  await app.close();
-});
+  afterAll(async () => {
+    await app.close();
+  });
 
-test('Testar credit', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .set('Cookie', [
-      'sessionId=88635993-7830-4eee-9b56-449ced1457fd; Max-Age=604800000; Path=/; Domain=localhost',
-    ])
-    .send({
-      nome: 'Teste',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201);
+  it('Testar credit', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        nome: 'Teste',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201);
+  });
+
+  it('List all transactions', async () => {
+    const cookiesLoged = await request(app.server)
+      .post('/transactions')
+      .send({
+        nome: 'Teste',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201);
+
+    const listAllTransaction = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookiesLoged.get('set-cookie'))
+      .expect(200);
+
+    expect(listAllTransaction.body.allTransactions.data).toEqual([
+      expect.objectContaining({
+        nome: 'Teste',
+        amount: 5000,
+      }),
+    ]);
+  });
 });
