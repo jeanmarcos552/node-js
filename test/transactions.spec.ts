a7e1af2c8a0ce272d1b456d51fb60d336e1c6956
Jean Marcos <jean.silva552@gmail.com>
2023-06-10 01:47:24 -0300

    feat: create tests summary and by ID

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -11,7 +11,7 @@ describe('Teste Routes transactions', () => {
     await app.close();
   });
 
-  it('Testar credit', async () => {
+  it('create new transactions credit', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -45,5 +45,63 @@ describe('Teste Routes transactions', () => {
         amount: 1000,
       }),
     ]);
+  });
+
+  it('shold be transaction ID', async () => {
+    const createOneTransaction = await request(app.server)
+      .post('/transactions')
+      .send({ nome: 'Teste', amount: 5000, type: 'credit' })
+      .expect(201);
+
+    const cookie = createOneTransaction.get('set-Cookie');
+
+    const listTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    const transactionID = listTransactions.body.allTransactions.data[0].id;
+
+    const responseTransaction = await request(app.server)
+      .get(`/transactions/${transactionID}`)
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(responseTransaction.body.transaction).toEqual(
+      expect.objectContaining({
+        nome: 'Teste',
+        amount: 5000,
+      })
+    );
+  });
+
+  it('shold be route summary', async () => {
+    const createTransactionCredit = await request(app.server)
+      .post('/transactions')
+      .send({
+        nome: 'Teste',
+        amount: 500,
+        type: 'credit',
+      })
+      .expect(201);
+
+    const cookie = createTransactionCredit.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookie)
+      .send({
+        nome: 'Teste',
+        amount: 200,
+        type: 'debit',
+      })
+      .expect(201);
+
+    const respSummary = await request(app.server)
+      .get('/transactions/resume')
+      .set('Cookie', cookie)
+      .expect(200);
+    console.log(respSummary.body.total);
+    expect(respSummary.body.total).toEqual(300);
   });
 });
